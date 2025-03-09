362f432d6fabc00a93acd13532eab5639eab04e1
Phan <bvphan0976@gmail.com>
2025-03-09 14:04:29 +0700

    Thiet lap moi truong lap trinh reactJS-FE, nodeJS-BE

---
+++ FrontEnd-reactjs-Vite/src/App.jsx
@@ -0,0 +1,23 @@
+import axios from '../ultill/axios.custom'
+import { useEffect, useState } from 'react'
+
+function App() {
+
+  // useEffect(() => {
+  //   const fectchHello = async () => {
+  // Goi API
+  // const res = await axios.get(`/`);
+  //     console.log(">>>Res : ", res);
+  //   }
+
+  //   fectchHello();
+  // }, [])
+
+  return (
+    <>
+      what ever
+    </>
+  )
+}
+
+export default App
